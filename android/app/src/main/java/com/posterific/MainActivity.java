2e29e0b6d7eb8fb7be89b5843b114059638db96f
Achmad Fatoni <achmadfatony@gmail.com>
2017-07-21 17:56:20 +0700

    configure fb sdk

--- android/app/src/main/java/com/posterific/MainActivity.java
+++ android/app/src/main/java/com/posterific/MainActivity.java
@@ -1,6 +1,7 @@
 package com.posterific;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Posterific";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getmCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
